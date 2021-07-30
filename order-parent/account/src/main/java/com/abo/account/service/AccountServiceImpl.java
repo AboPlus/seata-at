024e7f03ea0d1918b3edc8b38746c614676a220e
Abo <252841200@qq.com>
2021-07-30 17:10:45 +0800

    seata at 测试

--- order-parent/account/src/main/java/com/abo/account/service/AccountServiceImpl.java
+++ order-parent/account/src/main/java/com/abo/account/service/AccountServiceImpl.java
@@ -21,5 +21,11 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public void decrease(Long userId, BigDecimal money) {
         accountMapper.decrease(userId, money);
+
+        if (Math.random() < 0.5) {
+            throw new RuntimeException("模拟异常");
+        }
+
+
     }
 }
